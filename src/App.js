268e50b35c1e823d4bbac79ff12d0977a32b1159
AryanDhiman10 <97045861+AryanDhiman10@users.noreply.github.com>
2023-03-04 05:06:02 +0530

    Made Contact Us Page and Footer

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer"
+import Contact from "./components/contact"
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Contact />
+      <Footer />
+    </div>
+
+  );
+}
+
+export default App;
